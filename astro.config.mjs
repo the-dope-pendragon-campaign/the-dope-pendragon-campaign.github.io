3c831cc330c90dd4648330c4f02afa189d0fea78
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-22 17:49:53 -0400

    added site to config, added pages.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://the-dope-pendragon-campaign.github.io',
   integrations: [mdx(), tailwind()]
 });
